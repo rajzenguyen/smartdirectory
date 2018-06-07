9320902394b502c85f8653c16b1ca02b674dce35
Nguyen Mai Vu Thinh <thinh.nmv@nso.com.vn>
2018-06-07 09:10:31 +0700

    update

--- HTML/js/desktop.js
+++ HTML/js/desktop.js
@@ -4,12 +4,76 @@ $(document).ready(function(){
 	  centerPadding: '60px',
 	  slidesToShow: 3
 	});
+	
+	
 });
 $(document).ready(function(){
 	
 	$('.hideNoti').click(function(){
 		$('.hideNoti').parent(this).toggle("slow");
 	});
+	
+	
+	$('#lang').click(function(){
+		$('#menuLang').toggle();
+		
+	});
+	
+	$('#menuLang').click(function(){
+		$('#menuLang').toggle();
+		
+	});
+	
+	
+	/*change width base on number of elements*/
+	var element = document.getElementById("catContainer");
+	var numberOfElement = element.getElementsByClassName("catItem").length;
+	var odd=numberOfElement%4;
+	if(odd == 0)
+	{
+		for(i = 0; i<numberOfElement;i++){
+			element.getElementsByClassName("catItem")[i].style.width="22%";
+		}
+	}
+	else if(numberOfElement%5==0)
+	{
+		for(i = 0; i<numberOfElement;i++){
+			element.getElementsByClassName("catItem")[i].style.width="17%";
+		}
+	}
+	else if(odd == 1 && numberOfElement < 9){
+		for(i = 0; i<numberOfElement;i++){
+			element.getElementsByClassName("catItem")[i].style.width="17%";
+		}
+	}
+	else if(odd == 1 && numberOfElement >=9)
+	{
+		for(i = 0; i<numberOfElement-4;i++){
+			element.getElementsByClassName("catItem")[i].style.width="22%";
+		}
+		element.getElementsByClassName("catItem")[numberOfElement-5].style.width="18%";
+		element.getElementsByClassName("catItem")[numberOfElement-4].style.width="24%";
+		element.getElementsByClassName("catItem")[numberOfElement-3].style.width="14%";
+		element.getElementsByClassName("catItem")[numberOfElement-2].style.width="14%";
+		element.getElementsByClassName("catItem")[numberOfElement-1].style.width="15%";
+	}
+	else if(odd == 2)
+	{
+		for(i = 0; i<numberOfElement;i++){
+			element.getElementsByClassName("catItem")[i].style.width="17%";
+		}
+	}
+	else if(odd == 3)
+	{
+		for(i = 0; i<numberOfElement-3;i++){
+			element.getElementsByClassName("catItem")[i].style.width="22%";
+		}
+		element.getElementsByClassName("catItem")[numberOfElement-3].style.width="33%";
+		element.getElementsByClassName("catItem")[numberOfElement-2].style.width="20%";
+		element.getElementsByClassName("catItem")[numberOfElement-1].style.width="38%";
+	}
+	/*change width base on number of elements*/
+	
 	
 	var display1 = document.getElementById("companies");
 	var number1 = {param:0};
