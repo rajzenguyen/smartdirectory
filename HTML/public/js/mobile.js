e5735fddd4de3d081ebf05593eb98b0d146a1b17
Nguyen Mai Vu Thinh <thinh.nmv@nso.com.vn>
2018-06-26 16:04:50 +0700

    update code selection

--- HTML/public/js/mobile.js
+++ HTML/public/js/mobile.js
@@ -62,6 +62,15 @@ $(document).ready(function(){
 */
 $(document).ready(function(){
 	
+	$('.aboutSmartContainer').slick({
+		autoplay: false,
+		arrows:false,
+		centerMode: false,
+		variableWidth: false,
+		slidesToShow: 1
+	  });
+	  
+
 	$('.reviewSlick2').slick({
 	  dots: false,
 	  infinite: true,
@@ -131,8 +140,9 @@ $(document).ready(function(){
 
 });
 $(document).ready(function(){
-	if($(document).width() <= 1025)
+	/*if($(document).width() <= 1025)
 	{
+		
 		$('.staContent').slick({
 		dots: false,
 		infinite: false,
@@ -151,7 +161,45 @@ $(document).ready(function(){
 			arrows: false
 		});
 
-	}
+	}*/
+/*FUnction drag title */
+dragElement(document.getElementsById(("slideNav1")));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  $('#slideNav1header').onmousedown=dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = 0;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 });
 
 $(document).ready(function(){
